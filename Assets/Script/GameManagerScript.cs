b9b0ba8ea0bab230601666341d71b8de1df9240a
MrWinRock <mrwinrock11@gmail.com>
2024-04-05 02:11:35 +0700

    update: add dieUI in gameplayboss scene

--- Assets/Script/GameManagerScript.cs
+++ Assets/Script/GameManagerScript.cs
@@ -9,16 +9,19 @@ public class GameManagerScript : MonoBehaviour
     public GameObject gameOverUI;
     public GameObject RestartUI;
 
+    private Scene currentScene; // Declare the currentScene variable here
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("GMS Start");
+        currentScene = SceneManager.GetActiveScene(); // Get the current scene
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void gameOver()
@@ -26,32 +29,30 @@ public class GameManagerScript : MonoBehaviour
         // set gaveOverUI after die
         gameOverUI.SetActive(true);
     }
-    
+
     public void RestartClickedUI()
     {
         // restart the game
-        SceneManager.LoadScene("Train");
+        SceneManager.LoadScene(currentScene.buildIndex); // Load the current scene by its build index
     }
-    
+
     public void RestartButton()
     {
-        // go to ^&G%*&@#(R*%# after click restart button and wait for ^&G%*&@#(R*%#
+        // go to dying state after click restart button and wait for dying state
         RestartUI.SetActive(true);
         StartCoroutine(WaitForRestartCanvasEnd());
-        
     }
 
     public void MainMenuButton()
     {
         // go to main menu button after die
-        SceneManager.LoadScene("Train");
+        SceneManager.LoadScene(currentScene.buildIndex); // Load the current scene by its build index
     }
-    
+
     private IEnumerator WaitForRestartCanvasEnd()
     {
         // WaitForSeconds() <-- die animation lenght
         yield return new WaitForSeconds(0.64f);
         RestartClickedUI();
     }
-
 }
