ec4cf877f29b372be477e0013b7eec8af2c06cdb
Marguro <jarkkeryoki@gmail.com>
2024-05-13 14:00:38 +0700

    Add Fonts

--- Assets/Script/PopupController.cs
+++ Assets/Script/PopupController.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PopupController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject popup; // ตัวแปร GameObject สำหรับ popup
+    private bool isPlayerNearby = false; // ตัวแปรเก็บสถานะว่าผู้เล่นอยู่ใกล้ popup หรือไม่
+
+    // เมื่อมีการชนกับ Collider ของตัวละคร
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // ตรวจสอบว่าเป็นตัวละครหรือไม่
+        {
+            isPlayerNearby = true; // ตั้งค่าให้ผู้เล่นอยู่ใกล้ popup
+            popup.SetActive(true); // เปิด popup
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // เมื่อไม่มีการชนกับ Collider ของตัวละคร
+    private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // ตรวจสอบว่าเป็นตัวละครหรือไม่
+        {
+            isPlayerNearby = false; // ตั้งค่าให้ผู้เล่นไม่อยู่ใกล้ popup อีกต่อไป
+            popup.SetActive(false); // ปิด popup
+        }
+    }
+
+    // เมื่อมีการอัพเดตทุกๆ frame
+    private void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.Space)) // ตรวจสอบว่าผู้เล่นอยู่ใกล้ popup และกดปุ่ม Space หรือไม่
+        {
+            LoadNextScene(); // เรียกใช้ฟังก์ชันโหลด Scene ต่อไป
+        }
+    }
+
+    // ฟังก์ชันสำหรับโหลด Scene ต่อไป
+    private void LoadNextScene()
+    {
+        // โหลด Scene ต่อไปโดยใช้ SceneManager
+        SceneManager.LoadScene(2);
     }
 }
