7fa8d33594858a3be2c68cacbe41d5d5aa7223e6
vagolf <vagolf123@gmail.com>
2025-01-12 15:52:46 +0700

    import package playerMain

--- Assets/Script/Augment/AugmentImg.cs
+++ Assets/Script/Augment/AugmentImg.cs
@@ -34,6 +34,8 @@ public class AugmentImg : MonoBehaviour
 
     public void setAllImage(int aug1, int aug2, int aug3)
     {
+        Debug.Log($"In AugmentImg receive from GameManagerScript: {aug1}, {aug2}, {aug3}");
+
         setImage(0, aug1);
         setImage(1, aug2);
         setImage(2, aug3);
@@ -41,6 +43,10 @@ public class AugmentImg : MonoBehaviour
 
     private void setImage(int index, int aug)
     {
+        Debug.Log($"Setting augment image at index: {index}, with aug value: {aug}");
+        Debug.Log($"Length of augImages array: {augImages.Length}, Length of augTexts array: {augTexts.Length}");
+
+
         if (aug >= 1 && aug <= augImages.Length)
         {
             images[index].sprite = augImages[aug - 1];
--- Assets/Script/Mainmenu/PauseMenu.cs
+++ Assets/Script/Mainmenu/PauseMenu.cs
@@ -11,7 +11,7 @@ public class PauseMenu : MonoBehaviour
 
     void Update()
     {
-        if (optionMenuUi.activeSelf == true)
+        if (optionMenuUi.active == true)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
@@ -26,12 +26,10 @@ public class PauseMenu : MonoBehaviour
                 if (gameIsPause)
                 {
                     Resume();
-                    Debug.Log("Resume");
                 }
                 else
                 {
                     Pause();
-                    Debug.Log("Pause");
                 }
             }
         }
